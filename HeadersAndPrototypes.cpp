2b684014467386575157ad5987074a68668bc7cf
snowfire5492 <38293287+snowfire5492@users.noreply.github.com>
2015-01-20 15:12:12 -0800

    update

---
+++ HeadersAndPrototypes.cpp
@@ -0,0 +1,34 @@
+/*
+ *
+ *
+ *
+ *
+ */
+
+
+#include <iostream>			// <> usually used for already in place libraries
+
+#include "utils.h"			// "" usually used for files but it may not make a difference
+
+using namespace std;
+
+
+
+//void doSomething();				// PROTOTYPE ALLOWS YOU TO DEFINE FUNCTIONS LATER ON AND USE THEM PRIOR.
+								// PUT THESE ALL INSIDE OWN FILE
+
+
+int main(){
+
+	doSomething();
+	doSomething();
+
+	return 0;
+}
+
+
+
+void doSomething(){
+	cout << "hello" << endl;
+}
+
